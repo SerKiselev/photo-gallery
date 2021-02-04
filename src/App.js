7dfaacea5e50d7a8306399f101a9d68617104691
Sergey Kiselev <kiselev406@gmail.com>
2021-02-04 10:35:30 +0300

    initialize app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.scss';
+
+class App extends Component {
+  constructor(){
+      super()
+  }
+
+  render() {
+      return (
+          <div className = 'wrapper'>
+          </div>
+      )
+  }
+}
+
+export default App;
